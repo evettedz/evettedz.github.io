b481fb23bf21d17ff3bd955232f5d0fcaead354c
evettedz <90006630+evettedz@users.noreply.github.com>
2022-03-24 14:37:49 -0400

    Add files via upload

--- ungame/script.js
+++ ungame/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title.png", preGameScreenImageURLs: "./img/opening.png", postGameScreenImageURLs: "./img/ending.png" ,playUntilScore: 10, actionRadius: 100, 
+  mugen.initialize({titleScreenImageURL: "./img/title.png", preGameScreenImageURLs: "./img/opening.png", postGameScreenImageURLs: "./img/ending.gif" ,playUntilScore: 10, actionRadius: 100, 
     timeBetweenNPCs: 3200, removeNPCAfterInteracted: false, npcCollisionRadius: 100, 
-    actionDuration: 1000, npcSpeed: 400, typesOfNPC: 3, jumpHeight: 300, playUntilScore:10});
+    actionDuration: 500, npcSpeed: 400, typesOfNPC: 3, jumpHeight: 300, playUntilScore:10});
 });
